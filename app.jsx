7ba67ae0d9f9ebac0a84f926716f54585be83cdd
aydee <aydee.qm@gmail.com>
2017-10-06 13:08:34 -0500

    desarrollando la partye de teachers

--- app.jsx
+++ app.jsx
@@ -3,7 +3,7 @@ class Home extends React.Component {
       render() {
             return (
                   <div className="main-content home">
-                        <h2>Front End Course Directory</h2>
+                        <h2 className="title">Front End Course Directory</h2>
                         <p>This fun directory is a project for the <em>React Router Basics</em> course on Treehouse.</p>
                         <p>Learn front end web development and much more! This simple directory app offers a preview of our course
 					library. Choose from many hours of content, from HTML to CSS to JavaScript. Learn to code and get the
@@ -21,7 +21,7 @@ class About extends React.Component {
       render() {
             return (
                   <div className="main-content">
-                        <h2>About</h2>
+                        <h2 className="title">About</h2>
                         <p>The front end course directory lists many of the courses we teach on HTML, CSS, JavaScript and more! Be sure to
 					visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a
 					topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
@@ -32,12 +32,59 @@ class About extends React.Component {
 
 class Teachers extends React.Component {
       render() {
+            let teachersList = [
+                  {
+                        name: "Angie McAngular",
+                        img: "img/girl.png",
+                        text:"Angie is a web developer and teacher who is passionate about building scalable, data driven web apps, especially ones that address old problems with new tech!",
+                        id: 1
+                  },
+                  {
+                        name: "NodeStradamus",
+                        img: "img/boy1.png",
+                        text: "'NodeStra' is a software engineer and philosopher trying to leave the world better than he found it. He codes for non-profits, eCommerce, and large-scale web apps.",
+                        id: 2
+                  },
+                  {
+                        name: "Geo 'Lo' Cation",
+                        img: "img/boy2.png",
+                        text: "Geo is a JavaScript developer working on large-scale applications. He's also a teacher who strives to support students in removing all barriers to learning code.",
+                        id: 3
+                  },
+                  {
+                        name: "Ecma Scriptnstuff",
+                        img: "img/boy3.png",
+                        text: "Ecma found her passion for computers and programming over 15 years ago. She is excited to introduce people to the wonderful world of JavaScript.",
+                        id: 4
+                  },
+                  {
+                        name: "Jay Query",
+                        img: "img/boy4.png",
+                        text: "Jay is a developer, author of CSS: The Missing Manual, JavaScript & jQuery: The Missing Manual, and web development teacher.",
+                        id: 5
+                  },
+                  {
+                        name: "Json Babel",
+                        img: "img/boy5.png",
+                        text: "All of his professional life, Json has worked with computers online; he is a polyglot programmer and likes using the right tools for the job.",
+                        id: 6
+                  }
+            ]
+            let minsers = teachersList.map((item, index) => {
+                  return (
+                        <li key={index} className="teacher">
+                              <img className="teacher-img" src={item.img} alt=""/>
+                              <h3>{item.name}</h3>
+                              <p>{item.text}</p>
+                        </li>
+                  )
+            })
             return (
                   <div className="main-content">
-                        <h2>Teachers</h2>
-                        <p>The front end course directory lists many of the courses we teach on HTML, CSS, JavaScript and more! Be sure to
-					visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a
-					topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
+                        <h2 className="title">Teachers</h2>
+                        <ul className="group">
+                              {minsers}
+                        </ul>
                   </div>
             );
       }
@@ -68,16 +115,18 @@ class Repos extends React.Component {
             return (
                   <div className="main-content courses">
                         <div className="course-header group">
-                              <h2>REPOS</h2>
+                              <h2 className="title">Courses</h2>
                               <ul className="course-nav">
                                     <li><a href='#/repos/html'>HTML</a></li>
                                     <li><a href='#/repos/css'>CSS</a></li>
                                     <li><a href='#/repos/javascript'>JavaScript</a></li>
                               </ul>
+                              <div>
+                                    <ul>
+                                          {CurrentList}
+                                    </ul>
+                              </div>
 
-                              <ul>
-                                    {CurrentList}
-                              </ul>
                         </div>
 
                         {/* Write routes here... */}
